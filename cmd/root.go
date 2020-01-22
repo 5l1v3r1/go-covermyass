6abd13abcb3cbe6950fa69be2d1def126b956c46
sundowndev <raphael@crvx.fr>
2020-01-22 13:17:45 +0100

    feat: add auto confirm option flag

--- cmd/root.go
+++ cmd/root.go
@@ -7,15 +7,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+/*
+ * AutoConfirm Option used to skip user confirmation
+ */
+var AutoConfirm bool
+
 func init() {
+	// Register commands
 	rootCmd.AddCommand(clearCmd)
 	rootCmd.AddCommand(enableCmd)
 	rootCmd.AddCommand(disableCmd)
 	rootCmd.AddCommand(versionCmd)
+
+	// Register flags
+	clearCmd.PersistentFlags().BoolVarP(&AutoConfirm, "yes", "y", false, "Skip user confirmation")
+	enableCmd.PersistentFlags().BoolVarP(&AutoConfirm, "yes", "y", false, "Skip user confirmation")
+	disableCmd.PersistentFlags().BoolVarP(&AutoConfirm, "yes", "y", false, "Skip user confirmation")
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "covermyass",
+	Use:   "covermyass [COMMANDS] [OPTIONS]",
 	Short: "Cover your tracks on UNIX systems!",
 	Long:  `Shell script to cover your tracks on UNIX systems. Designed for pen testing "covering tracks" phase, before exiting the infected server. Or, permanently disable system logs for post-exploitation.`,
 }
