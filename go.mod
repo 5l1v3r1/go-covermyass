3728265b0641b1fe9c7b6e02424ac6bc744ec48b
sundowndev <raphael@crvx.fr>
2020-02-12 10:56:34 +0100

    feat: install goconvey

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/fatih/color v1.9.0
 	github.com/pelletier/go-toml v1.6.0 // indirect
+	github.com/smartystreets/goconvey v1.6.4
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v0.0.5
--- utils/tests/utils_test.go
+++ utils/tests/utils_test.go
@@ -4,16 +4,18 @@ import (
 	"os/user"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/sundowndev/go-covermyass/utils"
 )
 
 func TestGetUserHomeDir(t *testing.T) {
-	userProfile := &user.User{HomeDir: "/test"}
+	Convey("Should assert user home directory", t, func() {
+		userProfile := &user.User{HomeDir: "/test"}
 
-	UserCurrent := func() (*user.User, error) {
-		return userProfile, nil
-	}
+		UserCurrent := func() (*user.User, error) {
+			return userProfile, nil
+		}
 
-	assert.Equal(t, utils.GetUserHomeDir(UserCurrent), "/test", "they should be equal")
+		So(utils.GetUserHomeDir(UserCurrent), ShouldEqual, "/test")
+	})
 }
